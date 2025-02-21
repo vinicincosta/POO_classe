adb90974004f68c5a6cdc620d1fded19cb8a552f
vinicincosta <viniciusgcosta2603@gmail.com>
2025-02-21 12:31:31 -0300

    21/02

--- pessoa.py
+++ pessoa.py
@@ -18,18 +18,15 @@ class Pessoa:
 
     def apresentar(self):
         print(
-            f'Olá, sou {self.nome}.',
-            f'Data de nascimento: {self.data_nascimento}.',
-            f'Código: {self.codigo}.',
+            f' Olá, sou {self.nome}.\n',
+            f'Data de nascimento: {self.data_nascimento}.\n',
+            f'Código: {self.codigo}.\n',
             f'Situação atual:')
+        print("\n")
+        print('-' * 20)
 
         if self.estudando and self.trabalhando:
-            print(
-                f'Estudando e trabalhando. Salário atual: {self.salario} reais.')  # append Adiciona qualquer valor completo,
-            # por exemplo, se enviarmos um objeto,
-            # ele ad
-            # iciona o objeto, se enviarmos uma lista,
-            # ele adiciona a lista inteira ao invés de seus itens.
+            print(f'Estudando e trabalhando. Salário atual: {self.salario} reais.')
         elif self.estudando:
             print(f'Apenas estudando. Salário atual: {self.salario} reais.')
         elif self.trabalhando:
@@ -59,27 +56,24 @@ class Pessoa:
 
         # Testando a classe
 
-p1 = Pessoa('Vini', "26/03/2008", "1788768326")
-# p1.apresentar()
-
-
 
 class Bebe(Pessoa):
-    def __init__(self, nome, data_nascimento,codigo,
+    def __init__(self, nome, data_nascimento, codigo,
                  estudando=True, trabalhando=True):
-        super().__init__(nome, data_nascimento, codigo, estudando, trabalhando)
+        super().__init__(nome, data_nascimento,
+                         codigo, estudando, trabalhando)
         self.fome = True
         self.chorando = True
         self.dormindo = False
-        self.estudando =estudando
+        self.estudando = estudando
         self.trabalhando = trabalhando
+        print("\n")
+        print('-' * 20)
 
-
-
-    def situacao_bebe(self):
+    def apresentar(self):
         print(f'Olá, sou irmão do bebe {self.nome},\n'
-              f' meu irmão nasceu {self.data_nascimento},\n'
-              f' e seu registro é{self.codigo}.\n'
+              f'meu irmão nasceu {self.data_nascimento},\n'
+              f'e seu registro é:{self.codigo}.\n'
               f'situação atual:')
 
         if self.fome and self.chorando:
@@ -93,9 +87,8 @@ class Bebe(Pessoa):
 
         else:
             print('Ele está acordado')
-
-
-
+        print("\n")
+        print('-' * 20)
 
     def acordar(self):
         if self.dormindo:
@@ -105,16 +98,14 @@ class Bebe(Pessoa):
         else:
             print(f'bebe já está acordado')
 
-
     def mamar(self):
-       if self.fome:
-           self.chorando = False
-           self.fome= False
-           print(f'Bebê parou de chorar e não está mais com fome, pois já está satisfeito')
+        if self.fome:
+            self.chorando = False
+            self.fome = False
+            print(f'Bebê parou de chorar e não está mais com fome, pois já está satisfeito')
 
-       else:
-           print(f'bebe já mamou, ele já não tem mais fome')
-
+        else:
+            print(f'bebe já mamou, ele já não tem mais fome')
 
     def chorar(self):
         if self.chorando:
@@ -125,7 +116,6 @@ class Bebe(Pessoa):
         else:
 
             print(f'Bebê não está chorando, pois não está com fome')
-
 
     def dormir(self):
         if self.dormindo:
@@ -149,64 +139,10 @@ class Bebe(Pessoa):
             print(f'O bebe não está na creche ainda')
 
 
+s1 = Bebe('Dener', "26/03/2008", "1788768326")
+s1.apresentar()
 
-s1 = Bebe('Dener', "26/03/2008", "1788768326" )
-s1.situacao_bebe()
-
-s1.trabalhar()
-s1.estudar()
-s1.acordar()
-s1.dormir()
-s1.mamar()
-s1.mamar()
-s1.chorar()
-s1.dormir()
-s1.dormir()
-s1.dormir()
-s1.situacao_bebe()
-s1.trabalhar()
-
-
-
-
-# p2 = Pessoa('Dener', "05/10/2000", "9876543", )
-# p2.apresentar()
-#
-# p3 = Pessoa('Jão', "13/13/2013", "72345678", )
-# p3.apresentar()
-#
-# p4 = Pessoa('Luis', "31/02/1998", "28765432456", )
-# p4.apresentar()
-#
-# p5 = Pessoa('Leticia', '10/10/2014', "87654345678", )
-# p5.apresentar()
-
-
-# # class Curso:
-# #     def __init__(self, nome_curso, professor, aulas  ):
-# #         self.nome_curso = nome_curso
-# #         self.professor = professor
-# #         self.aulas = aulas
-# #
-# #
-# #     def apresentar_2(self):
-# #         print(f'Olá sou o professor(a) {self.professor}, '
-# #               f'minha matéria é {self.nome_curso} e '
-# #               f'aplico aulas {self.aulas}')
-# #
-# # c1 = Curso('Matemática', "Jean", "presenciais")
-# # c1.apresentar_2()
-# #
-# # c2 = Curso('Português', "Mateus", "onlines")
-# # c2.apresentar_2()
-# #
-# # c3 = Curso( 'História', "Dailson", 'presenciais')
-# # c3.apresentar_2()
-# #
-# # c4 = Curso ('Inglês', 'Nita', 'onlines')
-# # c4.apresentar_2()
-# #
-# # c5 = Curso('Geografia', "Chata", 'onlines')
-# c5.apresentar_2()
-
-#
+p1 = Pessoa('Vini', "26/03/2008", "1788768326")
+p1.apresentar()
+p1.estudar()
+p1.trabalhar()
